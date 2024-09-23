199b1c14676de7dbff6f49802392f47d5ac48cb0
Wayan Deva <wayaandeva123@gmail.com>
2024-09-23 12:17:49 +0700

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Politeknik, Politeknik Cikarang, Politeknik Takumi, Takumi, Kampus, Kampus Takumi, Kampus Cikarang, Cikarang, Kampus Terbaik, Kampus Terbaik di Jawa Barat, Kampus Terbaik di Cikarang, Kampus Terbaik di Bekasi, Politeknik Terbaik, Politeknik Terbaik di Jawa Barat, Politeknik Terbaik di Cikarang, Politeknik Terbaik di Bekasi, Penerimaan Mahasiswa Baru, Mahasiswa Takumi, Mahasiswa, Teknik Informatika, Prodi Teknik Informatika, Prodi TI, Teknik Informatika D3, Mekatronika, Mekatronika D3, Prodi Mekatronika, Mekatro, Bahasa Jepang, Bahasa Jepang D3, Prodi Bahasa Jepang, Digital Bisnis, Prodi Digital Bisnis, Sarjana Terapan, Sarjana Terapan Terbaik">
     <title>Politeknik Takumi</title>
 
     {{-- Alpine JS --}}
@@ -12,7 +13,6 @@
 
     {{-- Box Icon --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <!-- or -->
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 
     {{-- CSS --}}
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-    {{-- Tempat untuk Navbar --}}
+    {{-- Navbar --}}
     @include('layouts.navbar')
 
     {{-- Main --}}
